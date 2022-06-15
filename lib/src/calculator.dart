a25247f18e4364b9c11cdbc17aea010ff1242535
Yuba32 <92969369+Yuba32@users.noreply.github.com>
2022-06-15 13:54:13 +0900

    [update,add] setup

---
+++ lib/src/calculator.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'dart:math';
+import 'package:flutter_html/flutter_html.dart';
+// import 'package:math_keyboard/math_keyboard.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
